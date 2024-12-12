5516fa494efb54f6d3ff8eb55d700e0122618c3f
OleksandrTat <ifpoleksandrtataryn@gmail.com>
2024-12-12 21:06:01 +0100

    He optimizado el main1.js

--- objetos/js/main1.js
+++ objetos/js/main1.js
@@ -1,4 +1,4 @@
-let clases =[]
+let clases =[] // los clases para personaje
 
 class CrearClass{
     constructor(nombre, vida, defensa, ataque, magia, defensaMagica){
@@ -12,62 +12,51 @@ class CrearClass{
     }
 }
 
-const vagante = new CrearClass("vagante", 15, 11, 15, 9, 9);
-const guerrero = new CrearClass("guerrero", 11, 11, 10, 10, 8);
-const heroe = new CrearClass("heroe", 14, 12, 16, 7, 8);
-const bandido = new CrearClass("bandido", 10, 10, 9, 9, 8);
-const astrologo = new CrearClass("astrologo", 9, 9, 8, 16, 7);
-const profeta = new CrearClass("profeta", 10, 8, 11, 7, 16);
-const samurai = new CrearClass("samurai", 12, 13, 12, 9, 8);
-const prisionero = new CrearClass("prisionero", 11, 11, 11, 14, 6);
-const confesor = new CrearClass("confesor", 10, 10, 12, 9, 14);
-const miserable = new CrearClass("miserable", 10, 10, 10, 10, 10);
+const vagante = new CrearClass("Vagante", 15, 11, 15, 9, 9);
+const guerrero = new CrearClass("Guerrero", 11, 11, 10, 10, 8);
+const heroe = new CrearClass("Heroe", 14, 12, 16, 7, 8);
+const bandido = new CrearClass("Bandido", 10, 10, 9, 9, 8);
+const astrologo = new CrearClass("Astrologo", 9, 9, 8, 16, 7);
+const profeta = new CrearClass("Profeta", 10, 8, 11, 7, 16);
+const samurai = new CrearClass("Samurai", 12, 13, 12, 9, 8);
+const prisionero = new CrearClass("Prisionero", 11, 11, 11, 14, 6);
+const confesor = new CrearClass("Confesor", 10, 10, 12, 9, 14);
+const miserable = new CrearClass("Miserable", 10, 10, 10, 10, 10);
+const caballero = new CrearClass("Caballero", 13, 14, 12, 8, 12);
+const explorador = new CrearClass("Explorador", 12, 11, 10, 9, 11);
+const nigromante = new CrearClass("Nigromante", 9, 8, 10, 16, 14);
+const arquero = new CrearClass("Arquero", 11, 10, 13, 8, 9);
+const monje = new CrearClass("Monje", 10, 9, 9, 14, 13);
+const asesino = new CrearClass("Asesino", 12, 9, 14, 9, 10);
+const druida = new CrearClass("Druida", 9, 8, 9, 17, 15);
+const berserker = new CrearClass("Berserker", 16, 12, 18, 5, 7);
+const paladin = new CrearClass("Paladín", 14, 15, 13, 8, 13);
+const sombra = new CrearClass("Sombra", 10, 9, 15, 11, 9);
 
 
 
 
-let personajes = ["andrew", "alex", "ilias", "raul", "cayetano", "adrian", "olek", "camila", "vicky", "manuela", "leo", "steven", "lucia", "yohan", "carles"]
-
-let character = {
-    nombre: personajes[Math.floor(Math.random() * personajes.length)],
-    class: clases[Math.floor(Math.random() * clases.length)]
-}
 
 
-
-
-
-let randomNumNombre = null;
-let randomNumClase = null;
-let cantidadMax = null;
+let personajes = ["Andrew", "Alex", "Ilias", "Raul", "Cayetano", "Adrian", "Oleksandr", "Camila", "Vicky", "Manuela", "Leo", "Steven", "Lucia", "Yohan", "Carles"]
+let randomNum = null;
 const todosPersonajes = [];// aqui guardo todos personajes
+const personajesLength = personajes.length;
+const clasesLength = clases.length;
 
-if(personajes.length >= clases.length){ // aqui encuentro la variable con menor cantidad de elementos
-    cantidadMax = personajes.length
-}else{
-    cantidadMax = clases.length
+
+for(i=1; i<=personajesLength && i<=clasesLength; i++){
+    const Personaje = {};
+
+    randomNum = Math.floor(Math.random() * personajes.length);
+    Personaje.nombre = personajes[randomNum];
+    personajes.splice(randomNum, 1);
+
+    randomNum = Math.floor(Math.random() * clases.length);
+    Personaje.clase = clases[randomNum];
+    clases.splice(randomNum, 1);
+
+    todosPersonajes[`Personaje${i}${{}}`] = Personaje;
 }
-
-for(i=1; i<=cantidadMax; i++){
-    if(personajes.length > 0 && clases.length > 0){
-        randomNumNombre = Math.floor(Math.random() * personajes.length);
-        randomNumClase = Math.floor(Math.random() * clases.length);
-
-        const Personaje = {
-            nombre: personajes[randomNumNombre],
-            clase: clases[randomNumClase]
-        };
-
-        todosPersonajes[`Personaje${i}${{}}`] = Personaje;
-
-        personajes.splice(randomNumNombre, 1);
-        clases.splice(randomNumClase, 1);
-    }
-    else{break}
-}
-
-
 
 console.log(todosPersonajes);
-
-
